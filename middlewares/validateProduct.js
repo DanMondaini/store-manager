582668789dc1c0e89b850d00148f8c4aa2ca815f
DanMondaini <danielmondaini@outlook.com>
2022-03-01 19:36:08 -0300

    exclui alguns middlewares

--- middlewares/validateProduct.js
+++
@@ -1,9 +0,0 @@
-const validateProductId = (req, _res, next) => {
-    const { productId } = req.body;
-  
-    if (!productId) next({ status: 400, message: '"productId" is required' });
-  
-    next();
-  };
-  
-  module.exports = validateProductId;
